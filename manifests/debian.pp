ea35d3e906a7e53279c83cf0a1ef4c036b3ac47d
Marc Fournier <marc.fournier@camptocamp.org>
2013-04-27 11:33:39 +0200

    reprepro: make the module work for wheezy as well as ubuntu versions which have reprepro

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -10,45 +10,53 @@ class reprepro::debian {
   include reprepro::params
   include concat::setup
 
-  case $::lsbdistcodename {
-    squeeze: {
-      package {'reprepro':
-        ensure => 'latest';
-      }
+  if ($::operatingsystem == 'Debian' and $::operatingsystemrelease >= 6) {
+    $has_reprepro = true
+  }
+  elsif ($::operatingsystem == 'Ubuntu' and $::operatingsystemrelease >= 10) {
+    $has_reprepro = true
+  }
+  else {
+    $has_reprepro = false
+  }
 
-      group {'reprepro':
-        ensure => present,
-        system => true,
-      }
+  if $has_reprepro {
 
-      user {'reprepro':
-        ensure  => 'present',
-        home    => $reprepro::params::basedir,
-        shell   => '/bin/bash',
-        comment => 'reprepro base directory',
-        gid     => 'reprepro',
-        system  => true,
-        require => Group['reprepro'],
-      }
-
-      file {$reprepro::params::basedir:
-        ensure => directory,
-        owner  => 'reprepro',
-        group  => 'reprepro',
-        mode   => '0755',
-      }
-
-      file {"${reprepro::params::basedir}/.gnupg":
-        ensure => directory,
-        owner  => 'reprepro',
-        group  => 'reprepro',
-        mode   => '0700',
-      }
+    package {'reprepro':
+      ensure => 'latest';
     }
 
-    default: {
-      fail "reprepro is not available for ${operatingsystem}/${lsbdistcodename}"
+    group {'reprepro':
+      ensure => present,
+      system => true,
     }
+
+    user {'reprepro':
+      ensure  => 'present',
+      home    => $reprepro::params::basedir,
+      shell   => '/bin/bash',
+      comment => 'reprepro base directory',
+      gid     => 'reprepro',
+      system  => true,
+      require => Group['reprepro'],
+    }
+
+    file {$reprepro::params::basedir:
+      ensure => directory,
+      owner  => 'reprepro',
+      group  => 'reprepro',
+      mode   => '0755',
+    }
+
+    file {"${reprepro::params::basedir}/.gnupg":
+      ensure => directory,
+      owner  => 'reprepro',
+      group  => 'reprepro',
+      mode   => '0700',
+    }
+
+  } else {
+    fail "reprepro is not available for ${::operatingsystem}/${::lsbdistcodename}"
   }
 
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,8 @@
 class reprepro {
 
-  case $::operatingsystem {
-    Debian: {
-      case $::lsbdistcodename {
-        squeeze: { include reprepro::debian }
-        default: { fail "reprepro is not available for ${operatingsystem}/${lsbdistcodename}"}
-      }
-    }
-    default: { notice "Unsupported operatingsystem ${operatingsystem}" }
+  case $::osfamily {
+    'Debian': { include reprepro::debian }
+    default:  { notice "Unsupported osfamily ${::osfamily}" }
   }
 
 }
-
